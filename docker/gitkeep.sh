78c35a609a3c2850472cd423fe79d589c99947dd
Ketut Garjita <garjita@gmail.com>
2025-09-08 10:14:07 +0700

    add gitkeep.sh

---
+++ docker/gitkeep.sh
@@ -0,0 +1,10 @@
+touch airflow/airflow/logs/.gitkeep
+touch airflow/airflow/plugins/.gitkeep
+touch airflow_dags/.gitkeep
+touch airflow_logs/.gitkeep
+touch airflow_plugins/.gitkeep
+touch minio/seed/.gitkeep
+touch spark/extra-jars/.gitkeep
+touch spark_jobs/.gitkeep
+touch trino/etc/.gitkeep
+
